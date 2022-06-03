d7bd70e3c176006143b40511409a92ec6ea9343c
Katy14si <katianneantunescunha@gmail.com>
2022-06-03 17:33:21 -0300

    ok

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle)return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksOnToggle = tasks.map(task => task.id === id ?{
+    ...task,
+    isComplete: !task.isComplete,
+    } : task);
+
+    setTasks(tasksOnToggle)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const selectedTasks = tasks.filter(task => task.id != id)
+    setTasks(selectedTasks)
   }
 
   return (
@@ -68,6 +86,7 @@ export function TaskList() {
           ))}
           
         </ul>
+        
       </main>
     </section>
   )
